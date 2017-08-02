51e831edb6b91838ee2036018bbfc5f935be40e3
KKKKK5G <1210399613@qq.com>
2017-08-02 16:03:37 +0800

    Test

--- GitTest/Test/Program.cs
+++ GitTest/Test/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GitTest
+namespace Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Test");
         }
     }
 }
